b549d8e156d53babe5f1c2f3cbba099f881653c3
Spencer <backmanspencer99@gmail.com>
2019-07-29 16:35:32 -0500

    added footer and enterance animations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Tilt from 'react-tilt';
+import logo from './images/logo.svg';
+import chamberLogo from './images/chamberIcon.png';
+import devLogo from './images/ecoDevIcon.png';
+import tourismLogo from './images/tourismLogo.png';
+import line from './images/line.svg';
+import lineLeft from './images/line-left.svg';
+import lineRight from './images/line-right.svg';
+import './scss/app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="home-page">
+        <div id="fullscreen">
+          <iframe id="background-video" title="Grenada Drone Flyover" frameBorder="0" allowFullScreen src="https://player.vimeo.com/video/314553822?autoplay&loop&background=1"></iframe>
+        </div>
+        <div className="main-container">
+          <div className="header-container">
+            <img src={logo} className="main-logo" alt="Greater Grenada Partnership"/>
+            <div className="header-row">
+              <img src={lineLeft} className="side-line" alt="left line"/>
+              <h2 className="header2"> Welcome to the </h2>
+              <img src={lineRight} className="side-line" alt="right line"/>
+            </div>
+            <h1 className="header"> GREATER GRENADA PARTNERSHIP </h1>
+            <img className="main-underline" src={line} alt="Line"/>
+          </div>
+          <div className="tilt-container">
+            <Tilt className="Tilt" options={{ max: 25 }}>
+              <a href="http://www.grenadameansbusiness.com/" target="_blank" rel="noopener noreferrer" className="Tilt-inner">
+                <img src={devLogo} id="devLogo" alt="Grenada Economic Development District"/>
+              </a>
+            </Tilt>
+            <Tilt className="Tilt" options={{ max: 25 }}>
+              <a href="http://www.visitgrenadams.com/" className="Tilt-inner" target="_blank" rel="noopener noreferrer">
+                <img src={tourismLogo} id="tourismLogo" alt="Grenada Mississippi Tourism Commission"/>
+              </a>
+            </Tilt>
+            <Tilt className="Tilt" options={{ max: 25 }}>
+              <a href="https://www.grenadamississippi.com/" className="Tilt-inner" target="_blank" rel="noopener noreferrer">
+                <img src={chamberLogo} id="chamberLogo" alt="Grenada Area Chamber Of Commerce"/>
+              </a>
+            </Tilt>
+          </div>
+          <div className="footer-container">
+            <div className="footer-row">
+              <img src={lineLeft} className="footer-left-line" alt="Line-left"/>
+              <p className="footer-header"> Grenada Mississippi </p>
+              <img src={lineRight} className="footer-right-line" alt="Right-line"/>
+            </div>
+            <p className="footer-par">
+              A warm small town atmosphere with a community offering <br/> something for everyone yet always striving for more.
+              
+            </p>
+            <img src={line} className="footer-underline" alt="Underline"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
